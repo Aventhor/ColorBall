9300b56c2fe13ae7325cd84f052e18246d96f486
Aventhor <vaster399@gmail.com>
2018-10-09 20:12:52 +0300

    hello

---
+++ Scripts/Ball.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour {
+
+    [Header("Цвета шарика")]
+    public string[] colors;
+
+    public int Score
+    {
+        get { return score; }
+        set { value = 0; }
+    }
+    private int score;
+    private string currentColor;
+
+    public delegate void OnLoseGame();
+    public OnLoseGame callback;
+
+	// Use this for initialization
+	void Start () {
+        ChangeColor();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.CompareTag("Platform"))
+        {
+            if (coll.gameObject.GetComponent<Platform>().color == currentColor)
+            {
+                ChangeColor();
+                ChangeGravity();
+                score++;
+            }
+            else
+            {
+                //LoseGame();
+            }
+        }
+    }
+    private void ChangeColor()
+    {
+        Color color = new Color();
+        int randomNumber = Random.Range(0, 3);
+        ColorUtility.TryParseHtmlString(colors[randomNumber].ToString(), out color);
+        GetComponent<SpriteRenderer>().color = color;
+        currentColor = colors[randomNumber];
+    }
+    private void LoseGame()
+    {
+        callback();
+        GetComponent<Rigidbody2D>().simulated = false;
+    }
+    private void ChangeGravity()
+    {
+        float gravity = GetComponent<Rigidbody2D>().gravityScale;
+        if(gravity < 5.3)
+            gravity += 0.05f;
+        GetComponent<Rigidbody2D>().gravityScale = gravity;
+        print(gravity);
+    }
+}
---
+++ Scripts/Main.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+    [Header("Menu UI")]
+    public GameObject menuUI;
+    public Button playBtn;
+
+    [Header("Game UI")]
+    public GameObject gameUI;
+    public Canvas platfCanv;
+    public Text scoreText;
+    public GameObject resultPanel;
+    public Text scoreResult;
+
+    [Header("Префабы")]
+    public GameObject ball;
+    public GameObject platforms;
+
+    private Ball ballInstance;
+    private GameObject platformInstance;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameUI.activeSelf)
+        {
+            try
+            {
+                if (ballInstance != null)
+                {
+                    scoreText.text = ballInstance.Score.ToString();
+                }
+            }
+            catch
+            {
+                print("Waiting to creating ball");
+            }
+        }
+    }
+    public void Play()
+    {
+        playBtn.GetComponent<Animation>().Play();
+        StartCoroutine(OnExitMenu());
+    }
+    IEnumerator LoadMenu()
+    {
+        yield return new WaitForSeconds(0.5f);
+        menuUI.SetActive(true);
+
+    }
+    IEnumerator OnExitMenu()
+    {
+        yield return new WaitForSeconds(0.5f);
+        menuUI.SetActive(false);
+        StartCoroutine(OnStartGame());
+    }
+    IEnumerator OnStartGame()
+    {
+        if (!gameUI.activeSelf)
+        {
+            gameUI.SetActive(true);
+        }
+        if(resultPanel.activeSelf)
+        {
+            resultPanel.SetActive(false);
+        }
+        scoreText.text = "0";
+        GameObject p = Instantiate(platforms, new Vector2(0, -400), Quaternion.identity);
+        p.transform.SetParent(platfCanv.transform);
+        p.transform.localScale = new Vector2(88, 88);
+        p.transform.localPosition = new Vector3(0,-500,0);
+        platformInstance = p;
+        yield return new WaitForSeconds(1.5f);
+        GameObject o = Instantiate(ball, new Vector2(0, 3), Quaternion.identity);
+        ballInstance = o.GetComponent<Ball>();
+        ballInstance.callback += LoseGame;
+    }
+    public void LoseGame()
+    {
+        resultPanel.SetActive(true);
+        scoreResult.text = ballInstance.Score.ToString();
+        Destroy(ballInstance.gameObject);
+        Destroy(platformInstance);
+    }
+    public void Retry()
+    {
+        StartCoroutine(OnStartGame());
+    }
+    public void ExitMenu()
+    {
+        gameUI.SetActive(false);
+        StartCoroutine(LoadMenu());
+    }
+}
